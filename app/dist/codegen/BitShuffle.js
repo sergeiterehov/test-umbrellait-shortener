bebd9ce8d07ba789ee20f74e1803ba273910b6b9
Сергей Терехов <isergeiterehov@gmail.com>
2018-07-16 22:32:17 +0300

    Used es2018 for target in app

--- app/dist/codegen/BitShuffle.js
+++ app/dist/codegen/BitShuffle.js
@@ -1,31 +1,11 @@
-var __read = (this && this.__read) || function (o, n) {
-    var m = typeof Symbol === "function" && o[Symbol.iterator];
-    if (!m) return o;
-    var i = m.call(o), r, ar = [], e;
-    try {
-        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
-    }
-    catch (error) { e = { error: error }; }
-    finally {
-        try {
-            if (r && !r.done && (m = i["return"])) m.call(i);
-        }
-        finally { if (e) throw e.error; }
-    }
-    return ar;
-};
-var __spread = (this && this.__spread) || function () {
-    for (var ar = [], i = 0; i < arguments.length; i++) ar = ar.concat(__read(arguments[i]));
-    return ar;
-};
 /**
  * Generate shuffled vector
  * @param size Number of bits for shuffled vector
  */
 function generateArrangementVector(size) {
-    var vector = __spread(Array(size).keys());
-    var stack = __spread(Array(size).keys());
-    for (var i = 0; i < size; i++) {
+    const vector = [...Array(size).keys()];
+    const stack = [...Array(size).keys()];
+    for (let i = 0; i < size; i++) {
         vector[i] = stack.splice(Math.trunc(Math.random() * stack.length), 1)[0];
     }
     return vector;
@@ -35,15 +15,15 @@ function generateArrangementVector(size) {
  * @param vector Shuffled vector
  */
 function generateShuffleJs(vector) {
-    var sum = vector.map(function (a, b) { return "(((n >> " + a + ") & 0b1) << " + b + ")"; }).join(" +\n");
-    return "const bithash = (n) => (\n" + sum + "\n);";
+    const sum = vector.map((a, b) => `(((n >> ${a}) & 0b1) << ${b})`).join(" +\n");
+    return `const bithash = (n) => (\n${sum}\n);`;
 }
 if (!process.argv[2]) {
     throw new Error("Using: node {this-file.js} {size in bits}");
 }
-var bitsAmount = parseInt(process.argv[2], 10);
+const bitsAmount = parseInt(process.argv[2], 10);
 if (0 === bitsAmount) {
     throw new Error("Use non zero numbers");
 }
-var js = generateShuffleJs(generateArrangementVector(bitsAmount));
-process.stdout.write(js + "\n");
+const js = generateShuffleJs(generateArrangementVector(bitsAmount));
+process.stdout.write(`${js}\n`);
