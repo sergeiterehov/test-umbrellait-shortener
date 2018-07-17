4c2aac6ee15fc7db06e63bba10dbb24a4cc5b7c4
sterehov <sterehov@localhost>
2018-07-17 15:45:06 +0300

    dist removed from the repo

--- app/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const LinkHelper_1 = require("./helpers/LinkHelper");
-for (let i = 0; i < 30; i++) {
-    console.log(LinkHelper_1.LinkHelper.generateShuffleString(i));
-}
